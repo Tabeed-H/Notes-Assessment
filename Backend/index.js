f8711d8bcbaed307726b066eb88b37cfead7661d
tabeed-h <tabeedhameed121@gmail.com>
2024-01-27 13:24:11 +0530

    CORS6

--- Backend/index.js
+++ Backend/index.js
@@ -16,11 +16,31 @@ admin.initializeApp({
 app.use(cors({ origin: "*" }));
 app.use(express.json());
 
+const allowCors = (fn) => async (req, res) => {
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,OPTIONS,PATCH,DELETE,POST,PUT"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+  );
+  if (req.method === "OPTIONS") {
+    res.status(200).end();
+    return;
+  }
+  return await fn(req, res);
+};
+
 // Firestore Database Reference
 const db = admin.firestore();
 
 // API Route to Get All Notes
-app.get("/api/notes", async (req, res) => {
+app.get("/api/notes", allowCors, async (req, res) => {
   try {
     const notesSnapshot = await db.collection("notes").get();
     const notes = notesSnapshot.docs.map((doc) => ({
